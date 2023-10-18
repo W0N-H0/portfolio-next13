e066787fcda7c47579e994ed6b6196243c46eb80
WONHO22 <dnjsgh2122@gmail.com>
2023-10-19 03:12:33 +0900

    [Feat]Skills 컴포넌트 구현

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,7 +1,58 @@
 "use client";
+import { skills } from "@/constants/skills";
+import React, { useRef } from "react";
+import SectionTitle from "./SectionTitle/SectionTitle";
+import { motion, useInView } from "framer-motion";
 
 const Skills = () => {
-  return <div>Skills</div>;
+  const ref2 = useRef(null);
+  const isInView2 = useInView(ref2);
+
+  return (
+    <section
+      className="w-full h-fit px-5 py-10 max-xs:px-[20px] relative bg-white"
+      id="skills"
+    >
+      <div className="max-w-[1250px] mx-auto">
+        <SectionTitle title={"Skills"} />
+      </div>
+      <div ref={ref2} className="max-w-[1250px] mx-auto">
+        {skills.map((category, idx) => (
+          <motion.div
+            initial={{ x: "-20vw" }}
+            animate={{ x: isInView2 ? 0 : "-20vw" }}
+            transition={{ duration: 0.6 + (idx * 1.5) / 10 }}
+            key={idx}
+            className="px-5 py-5 max-xs:px-[20px]"
+          >
+            <h2 className="text-xl font-bold mb-2 mt-2 py-2 px-1 text-shadow">
+              - {category.category}
+            </h2>
+            <div className="grid grid-cols-5 gap-3">
+              {category.items.map((item) => (
+                <motion.div
+                  transition={{
+                    type: "spring",
+                    stiffness: 500,
+                    damping: 15,
+                  }}
+                  whileHover={{ scale: 1.1 }}
+                  key={item.id}
+                  className="p-4 rounded-md text-center flex flex-col items-center hover:shadow-2xl justify-center"
+                >
+                  <item.icon
+                    className="text-6xl"
+                    style={{ color: item.color }}
+                  />
+                  <span className="font-bold">{item.title}</span>
+                </motion.div>
+              ))}
+            </div>
+          </motion.div>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Skills;
---
+++ constants/skills.js
@@ -0,0 +1,79 @@
+import {
+  BiLogoReact,
+  BiLogoCss3,
+  BiLogoJavascript,
+  BiLogoNodejs,
+  BiLogoRedux,
+  BiLogoTailwindCss,
+} from "react-icons/bi";
+import { AiFillGithub, AiFillHtml5 } from "react-icons/ai";
+import { FiFigma } from "react-icons/fi";
+import {
+  SiExpress,
+  SiReactquery,
+  SiStyledcomponents,
+  SiPreact,
+} from "react-icons/si";
+import { BiLogoAws } from "react-icons/bi";
+import { TbBrandNextjs } from "react-icons/tb";
+import { CgVercel } from "react-icons/cg";
+import { v4 } from "uuid";
+
+export const skills = [
+  {
+    category: "Language",
+    items: [
+      { id: v4(), color: "#facc15", icon: BiLogoCss3, title: "CSS" },
+      { id: v4(), color: "#f59e0b", icon: AiFillHtml5, title: "HTML" },
+      {
+        id: v4(),
+        color: "#fcd34d",
+        icon: BiLogoJavascript,
+        title: "JavaScript",
+      },
+    ],
+  },
+  {
+    category: "Library & Framework",
+    items: [
+      { id: v4(), color: "#3b82f6", icon: BiLogoReact, title: "React" },
+      { id: v4(), color: "#3b82f6", icon: TbBrandNextjs, title: "Next.js" },
+      { id: v4(), color: "#166534", icon: BiLogoNodejs, title: "Node.js" },
+      { id: v4(), color: "#3b82f6", icon: SiExpress, title: "Express" },
+      { id: v4(), color: "#3b82f6", icon: SiReactquery, title: "React Query" },
+    ],
+  },
+  {
+    category: "State Management",
+    items: [
+      { id: v4(), color: "#764abc", icon: BiLogoRedux, title: "Redux Toolkit" },
+      { id: v4(), color: "#3b82f6", icon: SiPreact, title: "Context API" },
+    ],
+  },
+  {
+    category: "Style",
+    items: [
+      {
+        id: v4(),
+        color: "#facc15",
+        icon: SiStyledcomponents,
+        title: "Styled Components",
+      },
+      {
+        id: v4(),
+        color: "#3b82f6",
+        icon: BiLogoTailwindCss,
+        title: "Tailwind",
+      },
+      { id: v4(), color: "#764abc", icon: FiFigma, title: "Figma" },
+    ],
+  },
+  {
+    category: "Etc",
+    items: [
+      { id: v4(), color: "#0a0a0a", icon: AiFillGithub, title: "Git" },
+      { id: v4(), color: "#84cc16", icon: BiLogoAws, title: "AWS" },
+      { id: v4(), color: "black", icon: CgVercel, title: "Vercel" },
+    ],
+  },
+];
